17085e11790600293969e9f7af67e78b77517a04
jesninvence <jesninvence04@gmail.com>
2023-08-26 21:27:21 +0800

    Strap shop

--- Strap_ItemInfo.js
+++ Strap_ItemInfo.js
@@ -1,32 +1,3 @@
-// let main = document.getElementById("main-image");
-// let image1 = document.getElementById("image-1");
-// let image2 = document.getElementById("image-2");
-// let image3 = document.getElementById("image-3");
-// let image4 = document.getElementById("image-4");
-
-// image1.addEventListener("click", changeImage1);
-// image2.addEventListener("click", changeImage2);
-// image3.addEventListener("click", changeImage3);
-// image4.addEventListener("click", changeImage4);
-
-// function changeImage1(){
-//     main.removeAttribute("src");
-//     main.setAttribute("src", image1.getAttribute("src"));
-// }
-// function changeImage2(){
-//     main.removeAttribute("src");
-//     main.setAttribute("src", image2.getAttribute("src"));
-// }
-// function changeImage3(){
-//     main.removeAttribute("src");
-//     main.setAttribute("src", image3.getAttribute("src"));
-// }
-// function changeImage4(){
-//     main.removeAttribute("src");
-//     main.setAttribute("src", image4.getAttribute("src"));
-// }
-
-
 let main = document.getElementById("main-image");
 
 let thumbnail = document.querySelectorAll(".thumbnail");
--- main.js
+++ main.js
@@ -1,35 +1,101 @@
-const shadow = document.getElementById("shadow");
-const cartElement = document.getElementById("Cart");
+const global = {
 
-function showCartElement() {
-    cartElement.style.transform = "translateX(0)"
-    shadow.classList.remove("hide");
-    document.body.style.overflow = "hidden"
 }
 
-function hideCartElement() {
-    cartElement.style.transform = "translateX(100%)"
-    shadow.classList.add("hide");
-    document.body.style.overflow = "auto"
+function transBar() {
+    const navigation = document.querySelector("#header .navbar");
+    window.addEventListener("scroll", scrollTrans);
+    function scrollTrans() {
+        if (scrollY > 0) {
+            navigation.style.background = "black";
+        } else {
+            navigation.style.background = "transparent";
+        }
+    }
+
 }
 
-const sizes = document.querySelectorAll("#sizes button");
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+    const path = location.pathname;
+    switch(path) {
+        case "/Strap_shop.html":
+            const page = location.search.slice(1).split("=")[1];
+            showProducts(page);
+            break;
+        case "/Strap.html":
+            transBar();
+            break;
+        case "/Strap_Info.html":
+            displayInfo();
+            break;
+    }
+}
+async function showProducts(page = 1) {
+    const fetchProducts = await fetch("./products.json");
+    let products = await fetchProducts.json();
+    console.log(page)
+    products = products[page-1];
+    const product_display = document.getElementById("products_display");
+    let result = ``;
+    products.forEach(product => {
+        result += `
+            <div class="col-3">
+                <div class="card ${product.soldout && "soldout"} bg-white h-100">
+                    <a type="button" data-bs-toggle="modal" href="${product.link}" data-bs-target="#sampleModal1"><img src="${product.image}" width="100%"></a>
+                    ${product.soldout ? `<span class="tag"> Sold out </span>` : ""} 
+                    <div class="card-body">
+                        <h5 ${product.soldout ? `id="soldout"` : ""} class="card-title text-black">${product.name}</h5>
+                        <p class="card-price"><i class="fa-solid fa-peso-sign"></i> ${product.price} </p>
+                    </div>
+                </div>
+            </div>
+        `
+                    })
+    product_display.innerHTML = result
+}
+
+async function displayInfo(index) {
+    const fetchProducts = await fetch("./products.json");
+    let products = await fetchProducts.json();
+    console.log([1, 2].concat([3, 4]))
+
+    const sizes = document.querySelectorAll("#sizes button");
     for (let i = 0; i < sizes.length; i++) {
         const element = sizes[i];
-        element.addEventListener("click", activate)
+        element.addEventListener("click", activate);
     }
     function activate() {
-        const activated = document.querySelector("#sizes button.active")
-        this.classList.add("active")
-        activated.classList.remove("active")
+        const activated = document.querySelector("#sizes button.active");
+        this.classList.add("active");
+        activated.classList.remove("active");
     }
 
-const incrementBtn = document.querySelectorAll(".input-control")[1]
-const decrementBtn = document.querySelectorAll(".input-control")[0]
-incrementBtn.addEventListener("click", () => updateQuantity(1))
-decrementBtn.addEventListener("click", () => updateQuantity(-1))
-function updateQuantity(number) {
-    const quantity = document.getElementById("quantity")
-    quantity.value = Number(quantity.value) + number
+    const incrementBtn = document.querySelectorAll(".input-control")[1]
+    const decrementBtn = document.querySelectorAll(".input-control")[0]
+    incrementBtn.addEventListener("click", () => updateQuantity(1));
+    decrementBtn.addEventListener("click", () => updateQuantity(-1));
+    function updateQuantity(number) {
+        const quantity = document.getElementById("quantity");
+        quantity.value = Number(quantity.value) + number;
+    }
 }
 
+function inputCart() {
+    const shadow = document.getElementById("shadow");
+    const cartElement = document.getElementById("Cart");
+    
+    global.showCartElement = function() {
+        cartElement.style.transform = "translateX(0)"
+        shadow.classList.remove("hide");
+        document.body.style.overflow = "hidden"
+    }
+    
+    global.hideCartElement = function() {
+        cartElement.style.transform = "translateX(100%)"
+        shadow.classList.add("hide");
+        document.body.style.overflow = "auto"
+    }
+}
+
+inputCart()
